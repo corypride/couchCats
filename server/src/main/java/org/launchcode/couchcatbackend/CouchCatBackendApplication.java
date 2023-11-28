882c013fa8dd2409b8b1922d66d28b0182e089cb
Yumi <ydshiroma@gmail.com>
2023-11-27 21:10:33 -0500

    initial spring and react apps

---
+++ server/src/main/java/org/launchcode/couchcatbackend/CouchCatBackendApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.couchcatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchCatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouchCatBackendApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/org/launchcode/couchcatbackend/CouchCatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.couchcatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouchCatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
