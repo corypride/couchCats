d5aa0c30ea3f67882cad069ebd9a58694d3cea02
Eric Davidson <edavidson1410@gmail.com>
2023-12-01 17:17:05 -0600

    added NavBar component

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,16 @@
+import "../assets/css/LandingPage.css";
+import NavBar from "../components/NavBar";
+
+const LandingPage = () => {
+
+    return(
+        <>
+            <NavBar />
+            <div>
+                Hello World
+            </div>
+        </>
+    )
+}
+
+export default LandingPage;
