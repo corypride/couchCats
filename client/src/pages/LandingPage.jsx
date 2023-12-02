642f787d154e1a04c1835e786ab436db9edb5b31
Eric Davidson <edavidson1410@gmail.com>
2023-12-02 07:56:28 -0600

    chore: added .env file, build: created useFetch hook

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,16 +1,29 @@
 import { Button } from "@mui/material";
 import "../assets/css/LandingPage.css";
 import NavBar from "../components/NavBar";
+import useFetch from "../hooks/useFetch";
 
 const LandingPage = () => {
+
+    //discover why this is fetching 6 times per page load
+    const moviesOfDay = useFetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", process.env.REACT_APP_API_ACCESS_TOKEN);
+    console.log(moviesOfDay);
 
     return(
         <>
             <NavBar />
-            <div className="title">
-                CouchCat
+            <div className="hero">
+                <h2 className="title">
+                    CouchCat
+                </h2>
+                <h2 className="tagline">
+                    Spend your time watching.
+                </h2>
+                <Button variant="outlined">Find your Movie!</Button>
             </div>
-            <Button variant="outlined">Find your Movie!</Button>
+            <div className="moviesOfDay">
+
+            </div>
         </>
     )
 }
