459b38a1b1cf6defece5f9b4be1b6b708b955053
Eric Davidson <edavidson1410@gmail.com>
2023-12-03 13:02:52 -0600

    feat: added Movie of the Day; chore: added tmdb logo to assets

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -6,8 +6,9 @@ import useFetch from "../hooks/useFetch";
 const LandingPage = () => {
 
     //discover why this is fetching 6 times per page load
-    const moviesOfDay = useFetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", process.env.REACT_APP_API_ACCESS_TOKEN);
-    console.log(moviesOfDay);
+    const topMoviesRequest = useFetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", process.env.REACT_APP_API_ACCESS_TOKEN);
+    const topMovies = topMoviesRequest.data?.results;
+    console.log(topMovies)
 
     return(
         <>
@@ -21,8 +22,20 @@ const LandingPage = () => {
                 </h2>
                 <Button variant="outlined">Find your Movie!</Button>
             </div>
-            <div className="moviesOfDay">
-
+            <h1>Top Movies of the Day</h1>
+            <div className="topMovies">
+                {topMovies && topMovies.length > 0 ? (
+                    <>
+                    {topMovies.slice(0, 3).map((movie) => (
+                        <div className="topMovie">
+                            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie poster" />
+                                <h2>{movie.original_title}</h2>
+                                <h2>{movie.overview}</h2>
+                        </div>
+                    ))}
+                    </>
+                    ) : (<div>Loading top movies...</div> )
+                        }
             </div>
         </>
     )
