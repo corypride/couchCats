1414bbfe9052b124e362a654f210842d4a580bdf
erinmuzughi <erinmuzughi@gmail.com>
2023-12-05 16:56:32 -0600

    added email to user and registration form

--- server/src/main/java/org/launchcode/couchcatbackend/models/dto/RegisterFormDTO.java
+++ server/src/main/java/org/launchcode/couchcatbackend/models/dto/RegisterFormDTO.java
@@ -1,7 +1,12 @@
 package org.launchcode.couchcatbackend.models.dto;
 
-public class RegisterFormDTO extends org.launchcode.couchcatbackend.models.dto.LoginFormDTO {
+import jakarta.validation.constraints.Email;
 
+public class RegisterFormDTO extends org.launchcode.couchcatbackend.models.dto.LoginFormDTO {
+//The Registration form extends the login form because it shares two of the same fields: username and password.
+// We then require the user to fill in two additional fields here, an email and a second verify password field.
+    @Email
+    private String email;
     private String verifyPassword;
 
     public String getVerifyPassword() {
@@ -12,4 +17,11 @@ public class RegisterFormDTO extends org.launchcode.couchcatbackend.models.dto.L
         this.verifyPassword = verifyPassword;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
