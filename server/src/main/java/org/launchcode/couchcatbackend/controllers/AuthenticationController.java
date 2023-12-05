722d6d68c3899918d272cc33c6a8d11142423969
erinmuzughi <erinmuzughi@gmail.com>
2023-12-05 17:36:09 -0600

    outlined to do's for controller

--- server/src/main/java/org/launchcode/couchcatbackend/controllers/AuthenticationController.java
+++ server/src/main/java/org/launchcode/couchcatbackend/controllers/AuthenticationController.java
@@ -3,10 +3,30 @@ package org.launchcode.couchcatbackend.controllers;
 import org.launchcode.couchcatbackend.data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthenticationController {
 
     @Autowired
     UserRepository userRepository;
+
+    @GetMapping("hello")
+    @ResponseBody
+    public String hello() {
+        return "Hello, Spring!";
+    }
+
+    //TO DO: Session handling methods that create the session ID and cookie which allows us to store and retrieve the login status of a user in a session / a logged-in user’s user ID will be stored in their session.
+    //   We also need to receive the cookie from the front end as the user accesses restricted pages to validate the user against the information stored for the user's session to verify they are logged in/can access those pages.
+
+    // TO DO: PostMapping Method to Process Registration Form,
+    // validate the entries and either send back error messages or create a new user and send back a success message
+
+
+    // TO DO: PostMapping Method to Process Login Form,
+    // validate the username and password are accurate/match what we have stored in the database for the user
+
+
 }
