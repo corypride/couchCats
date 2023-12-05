eb96e372bed39de8be93c25069f8c725ef6390b5
Eric Davidson <edavidson1410@gmail.com>
2023-12-04 21:03:18 -0600

    savepoint: landing page, feat: search bar for nav needs work

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,24 +1,59 @@
+import { useEffect, useState } from "react";
 import "../assets/css/NavBar.css";
-import Autocomplete from '@mui/material/Autocomplete';
-import TextField from '@mui/material/TextField';
+import { Autocomplete, TextField } from '@mui/material';
+import useFetch from "../hooks/useFetch";
 
 const NavBar = () => {
 
-    const numbers = ["1", "2", "3"]
+    // Fix the AutoComplete
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState([])
+
+    useEffect(() => {
+        const handleSearch = async () => {
+          if (searchTerm.length > 0) {
+            const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}`);
+            const data = await response.json();
+            setSearchResults(data.results);
+          }
+        };
+        handleSearch();
+      }, [searchTerm]);
+
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value);
+        console.log(searchTerm)
+    }
+
+    const handleSelect = (movie) => {
+        console.log(searchResults)
+        setSearchTerm(movie.original_title);
+        console.log(searchTerm)
+        setSearchResults([]);
+      };
+
 
     return(
         <div className="navBar">
             <div className="logo">
                 Cc
             </div>
-            <div className="searchBar">
+            <div>
                 <Autocomplete
-                    id="free-solo-demo"
-                    size="medium"
+                    id="search"
                     freeSolo
-                    options={numbers.map((option) => option)}
-                    renderInput={(params) => <TextField {...params} label="Search Movies" />}
-                    />
+                    options={searchResults}
+                    getOptionLabel={(option) => option.original_title}
+                    renderOption={(option) => (
+                        <li key={option.id} onClick={() => handleSelect}>
+                            {option.key}
+                            </li>
+                      )}
+                    renderInput={(params) => (
+                        <TextField {...params} label="Search Movies..." variant="outlined" fullWidth onChange={handleChange} />
+                    )}
+                />
+                <div>Log in</div>
             </div>
         </div>
     )
--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -8,7 +8,6 @@ const LandingPage = () => {
     //discover why this is fetching 6 times per page load
     const topMoviesRequest = useFetch("https://api.themoviedb.org/3/movie/popular?language=en-US&page=1", process.env.REACT_APP_API_ACCESS_TOKEN);
     const topMovies = topMoviesRequest.data?.results;
-    console.log(topMovies)
 
     return(
         <>
@@ -27,7 +26,7 @@ const LandingPage = () => {
                 {topMovies && topMovies.length > 0 ? (
                     <>
                     {topMovies.slice(0, 3).map((movie) => (
-                        <div className="topMovie">
+                        <div key={movie.id} className="topMovie">
                             <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie poster" />
                                 <h2>{movie.original_title}</h2>
                                 <h2>{movie.overview}</h2>
