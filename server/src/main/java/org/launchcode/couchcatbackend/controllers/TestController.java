cc31f10c168c1d2b5a73cdb1421e39238b507010
erinmuzughi <erinmuzughi@gmail.com>
2023-12-05 21:40:36 -0600

    testing post and trying to figure out why I'm receiving a 405

--- server/src/main/java/org/launchcode/couchcatbackend/controllers/TestController.java
+++ server/src/main/java/org/launchcode/couchcatbackend/controllers/TestController.java
@@ -6,6 +6,8 @@ import org.launchcode.couchcatbackend.data.UserRepository;
 import org.launchcode.couchcatbackend.models.User;
 import org.launchcode.couchcatbackend.models.dto.RegisterFormDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin(origins = "*", methods = {RequestMethod.POST})
 @RequestMapping("test-api")
 public class TestController {
 
@@ -24,7 +27,8 @@ public class TestController {
         return userRepository.findById(id);
     }
 
-//    @PostMapping("/register")
-//
-//
-//    }
+    @PostMapping("/register")
+    public ResponseEntity<String> testRegister(@RequestBody String requestBody){
+        return new ResponseEntity<>("Yata", HttpStatus.OK);
+    }
+}
