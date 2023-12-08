e5aae535eeaf255d4cd1714f2af0b4636c70be94
erinmuzughi <erinmuzughi@gmail.com>
2023-12-07 20:46:15 -0600

    changed user so we are just using password and not pwHash for now

--- server/src/main/java/org/launchcode/couchcatbackend/controllers/AuthenticationController.java
+++ server/src/main/java/org/launchcode/couchcatbackend/controllers/AuthenticationController.java
@@ -19,11 +19,9 @@ public class AuthenticationController {
     public User register(@RequestBody User newUser) {
         return newUser; //not receiving the pw back from postman, need to figure that out
 
-//        return (userService.saveUser(newUser));
+//        return (userService.saveUser(newUser)); //keeping as reference in case I end up using a Service class
     }
 
-    //TO DO: Session handling methods that create the session ID and cookie which allows us to store and retrieve the login status of a user in a session / a logged-in user’s user ID will be stored in their session.
-    //   We also need to receive the cookie from the front end as the user accesses restricted pages to validate the user against the information stored for the user's session to verify they are logged in/can access those pages.
 
     // TO DO: PostMapping Method to Process Registration Form,
     // validate the entries and either send back error messages or create a new user and send back a success message
@@ -31,6 +29,9 @@ public class AuthenticationController {
 
     // TO DO: PostMapping Method to Process Login Form,
     // validate the username and password are accurate/match what we have stored in the database for the user
+
+    //TO DO: Session handling methods that create the session ID and cookie which allows us to store and retrieve the login status of a user in a session / a logged-in user’s user ID will be stored in their session.
+    //   We also need to receive the cookie from the front end as the user accesses restricted pages to validate the user against the information stored for the user's session to verify they are logged in/can access those pages.
 
 
 }
