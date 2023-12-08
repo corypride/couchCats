1a858b731b38da053b55a923aa734f5282ee4429
Yumi <ydshiroma@gmail.com>
2023-12-08 15:32:06 -0500

    clean up documentation

--- server/src/main/java/org/launchcode/couchcatbackend/controllers/MovieController.java
+++ server/src/main/java/org/launchcode/couchcatbackend/controllers/MovieController.java
@@ -35,7 +35,7 @@ public class MovieController {
         return movieRepository.findById(id).orElseThrow(RuntimeException::new);
     }
 
-//    Return all movies on a user's watchlist at /watchlist/{userid}
+//    Return all movies on a user's watchlist at /movies/watchlist/{userid}
     @GetMapping(value = "/watchlist/{userid}")
     public List<Movie> getWatchlist(@PathVariable int userid) {
         Optional<User> result = userRepository.findById(userid);
@@ -43,13 +43,14 @@ public class MovieController {
         return user.getWatchlist();
     }
 
-//    Add movie to database
+//    Add movie to database at /movies
     @PostMapping
     public void saveMovie(@RequestBody Movie movie) {
         movieRepository.save(movie);
     }
 
 //    TODO: check if this works (I'm not sure how)
+//    Save movie to watchlist at /movies/save
     @PostMapping(path = "save")
     public void saveMovieToWatchlist(@RequestBody UserMovieDTO userMovieDTO) {
         Movie movie = userMovieDTO.getMovie();
@@ -59,7 +60,7 @@ public class MovieController {
         movieRepository.save(movie);
     }
 
-//    Delete one movie from database at /delete/{id}
+//    Delete one movie from database at /movies/delete/{id}
 //    TODO: this isn't working. permissions/authorization issue?
 //    TODO: do I need to check if a movie with that ID exists first?
     @DeleteMapping(value = "/delete/{id}")
