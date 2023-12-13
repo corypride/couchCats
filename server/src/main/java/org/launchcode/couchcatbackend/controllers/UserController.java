1d7e40e400d62ed43d6db374e0d65ad280ae9275
erinmuzughi <erinmuzughi@gmail.com>
2023-12-12 21:26:56 -0600

    code cleanup and comments

--- server/src/main/java/org/launchcode/couchcatbackend/controllers/UserController.java
+++ server/src/main/java/org/launchcode/couchcatbackend/controllers/UserController.java
@@ -7,9 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.Optional;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.client.HttpClientErrorException;
 
 
 @RestController
@@ -23,15 +21,19 @@ public class UserController {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    //For adding a user: checks if a user with that email already exists and returns BAD REQUEST/400 error w/ custom body if so,
-    // otherwise, the user is created, the password is encoded and a CREATED/201 HTTP response is returned
+    /* For registering a new user:
+        checks if a user with that email already exists:
+            if yes: a BAD REQUEST/400 HTTP response is returned w/ a custom body;
+            otherwise: the user is created, the password is encoded, they are saved to the database,
+                and a CREATED/201 HTTP response is returned w/ a custom body */
 
     @PostMapping(value = "/register", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<String> register(@RequestBody User user) {
         User isExist = (userRepository.findByEmail(user.getEmail()));
         if (isExist != null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User with email " + user.getEmail() + " already exists. Either a new email to register.");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("User with email " + user.getEmail() + " already exists. Either a new email to register.\n");
         }
+
         User newUser = new User();
 
         newUser.setFirstName(user.getFirstName());
@@ -40,8 +42,7 @@ public class UserController {
         newUser.setPassword(passwordEncoder.encode(user.getPassword()));
 
         userRepository.save(newUser);
-        return ResponseEntity.status(HttpStatus.CREATED).body("HTTP Status will be CREATED (CODE 201)\n");
-
+        return ResponseEntity.status(HttpStatus.CREATED).body("User was successfully registered.\n");
     }
 }
 
