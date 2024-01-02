6c25de5808f2608f110fbb5e94276cebe02dca31
edavidson1410 <129311010+edavidson1410@users.noreply.github.com>
2024-01-02 09:15:36 -0600

    feat: watchList add button styling based off selected

--- client/src/components/MovieDisplay.jsx
+++ client/src/components/MovieDisplay.jsx
@@ -4,7 +4,6 @@ import getCastCrew from "../utils/getCastCrew"
 import getServices from "../utils/getServices"
 import { useEffect, useState } from 'react';
 import streamingServices from '../assets/streamingServices';
-import { ContentCutOutlined } from '@mui/icons-material';
 
 const MovieDisplay = (props) => {
 
@@ -12,9 +11,10 @@ const MovieDisplay = (props) => {
   const [castCrew, setCastCrew] = useState();
   const [director, setDirector] = useState();
   const [topCast, setTopCast] = useState();
+  const [selected, setSelected] = useState(false);
 
   // adds movie to watchlist
-  async function handleListAdd(movie) {
+  async function handleListAdd(movie, index) {
     try {
 
       const url = 'http://localhost:8081/watchlist/save'; // Replace with the actual API endpoint
@@ -23,19 +23,22 @@ const MovieDisplay = (props) => {
         userId: 1,
         movie: {
           id: movie.id,
-          description: movie.overview,
-          poster: movie.poster_path,
-          year: parseInt(movie.release_date.slice(0,4)),
           title: movie.title,
+          year: parseInt(movie.release_date.slice(0,4)),
+          description: movie.overview,
+          //TODO: need to change directors into string if there are multiple
           director: director.name,
-          cast: 'placeholder',
-          rating: movie.vote_average
+          //TODO: need to change cast names into string
+          cast: 'placeholder', 
+          rating: movie.vote_average,
+          poster: movie.poster_path
         }
       };
-  
       const response = await axios.post(url, movieData);
       console.log('Response:', response.data);
+      // setSelected(!selected); Actual place of selected
     } catch (error) {
+      setSelected(!selected);
       console.error('Error:', error);
     }
   }
@@ -53,11 +56,9 @@ const MovieDisplay = (props) => {
   }, [props.movie])
 
   useEffect(() => {
-    // console.log(castCrew.crew.filter(({job})=> job ==='Director'))
     if(castCrew) {
       setDirector(castCrew.crew.filter(({job})=> job ==='Director'))
       setTopCast(castCrew.cast.slice(0,3))
-      console.log(topCast)
     } 
   }, [castCrew])
 
@@ -77,27 +78,39 @@ const MovieDisplay = (props) => {
         sx={{
           display: "flex",
           justifyContent: "center",
-
         }}>
+          {/* movie poster and add button-left side*/}
           <Box component="div">
-{/* movie poster */}
-            <Box 
-              sx={{
-                height: "35rem",
-                width: "25rem",
-              }}
-              component="img" 
-              src={`https://image.tmdb.org/t/p/w500${props.movie.poster_path}`} 
-              alt="movie poster" />
-            <ListItemButton
-              variant="cont"
-              onClick={() => handleListAdd(props.movie)}
-              sx={{
-                flexGrow: "0",
-                bgcolor: "accent.main"
-              }}
-              >Add</ListItemButton>
+                {/* movie poster */}
+                <Box 
+                  sx={{
+                    height: "35rem",
+                    width: "25rem",
+                  }}
+                  component="img" 
+                  src={`https://image.tmdb.org/t/p/w500${props.movie.poster_path}`} 
+                  alt="movie poster" />
+                <ListItemButton
+                  variant="cont"
+                  onClick={() => handleListAdd(props.movie)}
+                  selected={selected}
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    bgcolor: "accent.main",
+                    width: "6rem",
+                    "&.Mui-selected": {
+                      backgroundColor: "primary.main", // Apply custom styling when selected
+                      color: "white",
+                    },
+                    "&:hover": {
+                      backgroundColor: "accent.secondary",
+                      color: "white" // Hover styles override selected background
+                    },
+                  }}
+                  >{selected ? "Added" : "Add"}</ListItemButton>
           </Box>
+          {/* right side */}
           <Box 
           component="div"
           sx={{
@@ -105,71 +118,101 @@ const MovieDisplay = (props) => {
             flexDirection: "column",
             height: "50%",
             alignSelf: "flex-start",
+            padding: "2rem"
           }}>
-            <Box component="div"
-            sx={{
-              display: "flex",
-              gap: "1rem"
-            }}>
-{/* movie title and release year*/}
-              <Typography
+                {/* movie title and release year-top*/}
+                <Box component="div"
                 sx={{
-                  alignSelf: "flex-start",
-                  fontSize: "2.5rem",
-                  fontWeight: "bold"
-                }}
-                >{props.movie.original_title}</Typography>
-              <Typography
-              sx={{
-                alignSelf: "center"
-              }}>{props.movie.release_date.slice(0,4)}</Typography>
-            </Box>
-{/* overview */}
-              <Typography
-              sx={{
-              }}>{props.movie.overview}</Typography>
-{/* director */}
-              <Typography
-              sx={{
-                fontSize: "3rem"
-              }}>Director</Typography>
-              {director ? 
-                <>
-                  {director.map((item) => (
+                  display: "flex",
+                  gap: "1rem",
+                  marginBottom: "1rem"
+                }}>
                   <Typography
-                  key={item.name}>{item.name}</Typography>
-                ))}
-                </> 
-                : "No Director"}
-{/* Top Billed Cast */}
-              <Typography
-              sx={{
-                fontSize: "3rem"
-              }}>Top Billed</Typography>
-              {director ? 
-                <>
-                  {topCast.map((item) => (
-                  <Typography
-                  key={item.name}>{item.name}</Typography>
-                ))}
-                </> 
-                : "No Top Billed"}
-{/* streaming services */}
-              {services ? 
-              <Box> 
-                {services.map((service) => (
-                  <Box 
-                    key={service.provider_name}
                     sx={{
-                      height: "4rem",
-                      width: "4rem",
-                      borderRadius: "5rem"
+                      alignSelf: "flex-start",
+                      fontSize: "2.5rem",
+                      fontWeight: "bold"
                     }}
-                    component="img" 
-                    src={`https://image.tmdb.org/t/p/original/${service.logo_path}`} alt="stream logo" />
-                ))}
-              </Box>
-              : "Not on Streaming...."}
+                    >{props.movie.original_title}</Typography>
+                  <Typography
+                  sx={{
+                    alignSelf: "center",
+                    color: "accent.main"
+                  }}>{props.movie.release_date.slice(0,4)}</Typography>
+                </Box>
+                {/* overview-middle */}
+                <Typography
+                sx={{
+                  marginBottom: "1rem",
+                  color: "accent.main"
+                }}>{props.movie.overview}</Typography>
+                {/* director and top billed cast-middle */}
+                <Box sx={{
+                  display: "flex",
+                  gap: "3rem",
+                  marginBottom: "1rem"
+                }}>
+                    {/* director */}
+                    <Box 
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column"
+                    }}>
+                        <Typography
+                        sx={{
+                          fontSize: "3rem"
+                        }}>Director</Typography>
+                        {director ? 
+                          <>
+                            {director.map((item) => (
+                            <Typography
+                            key={item.name}
+                            sx={{
+                              color: "accent.main"
+                            }}>{item.name}</Typography>
+                          ))}
+                          </> 
+                          : "No Director"}
+                    </Box>
+                    {/* Top Billed Cast */}
+                    <Box 
+                    sx={{
+                      display: "flex",
+                      flexDirection: "column"
+                    }}>
+                        <Typography
+                        sx={{
+                          fontSize: "3rem"
+                        }}>Cast</Typography>
+                        {topCast ? 
+                          <>
+                            {topCast.map((item) => (
+                            <Typography
+                            key={item.name}
+                            sx={{
+                              color: "accent.main"
+                            }}>{item.name}</Typography>
+                          ))}
+                          </> 
+                          : "No Top Billed"}
+                    </Box>
+                </Box>  
+                {/* streaming services-bottom */}
+                {services ? 
+                <Box> 
+                  {services.map((service) => (
+                    <Box 
+                      key={service.provider_name}
+                      sx={{
+                        height: "4rem",
+                        width: "4rem",
+                        borderRadius: "5rem"
+                      }}
+                      component="img" 
+                      src={`https://image.tmdb.org/t/p/original/${service.logo_path}`} alt="stream logo" />
+                  ))}
+                  </Box>
+                  : "Not on Streaming...."}
           </Box>
         </ListItem>
       </List>
